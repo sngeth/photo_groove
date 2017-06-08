8b2fcafcd6ff9287063c53d42c5ad19827082455
Sid Ngeth <sngeth@gmail.com>
2017-06-07 21:53:10 -0400

    Add thumbnail size picker and random button

--- PhotoGroove.elm
+++ PhotoGroove.elm
@@ -2,7 +2,30 @@ module PhotoGroove exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Array exposing (Array)
 
+type alias Photo =
+    { url : String }
+
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
+    , chosenSize : ThumbnailSize
+    }
+
+
+type alias Msg =
+    { operation : String, data : String }
+
+
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
+
+
+initialModel : Model
 initialModel =
     { photos =
         [ { url = "1.jpeg" }
@@ -10,13 +33,25 @@ initialModel =
       , { url = "3.jpeg" }
       ]
     , selectedUrl = "1.jpeg"
+    , chosenSize = Medium
     }
 
 
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+
+selectPhoto : { operation : String, data : String }
+selectPhoto = { operation = "SELECT_PHOTO", data = "1.jpeg" }
+
+
+urlPrefix : String
 urlPrefix =
-  "http://elm-in-action.com/"
+    "http://elm-in-action.com/"
 
 
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumbnail =
     img [ src (urlPrefix ++ thumbnail.url)
         , classList [ ( "selected", selectedUrl == thumbnail.url ) ]
@@ -25,29 +60,62 @@ viewThumbnail selectedUrl thumbnail =
         []
 
 
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+    label []
+        [ input [ type_ "radio", name "size" ] []
+        , text (sizeToString size)
+        ]
+
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+      Small ->
+          "small"
+
+      Medium ->
+          "med"
+
+      Large ->
+          "large"
+
+
+update : Msg -> Model -> Model
 update msg model =
-  if msg.operation == "SELECT_PHOTO" then
-    { model | selectedUrl = msg.data }
-  else
-      model
+    case msg.operation of
+        "SELECT_PHOTO" ->
+            { model | selectedUrl = msg.data }
+
+        "SUPRISE_ME" ->
+            { model | selectedUrl = "2.jpeg" }
+
+        _ ->
+            model
 
 
+view : Model -> Html Msg
 view model =
-  div [ class "content" ]
-      [ h1 [] [ text "Photo Groove" ]
-      , div [ id "thumbnails" ]
-          (List.map (viewThumbnail model.selectedUrl) model.photos)
-      , img
-          [ class "large"
-          , src (urlPrefix ++ "large/" ++ model.selectedUrl)
-          ]
-          []
-      ]
-
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Groove" ]
+        , button
+            [ onClick { operation = "SUPRISE_ME", data = "" } ]
+            [ text "Suprise Me!" ]
+        , h3 [] [ text "Thumbnail Size:" ]
+        , div [ id "choose-size" ]
+            (List.map viewSizeChooser [ Small, Medium, Large ])
+        , div [ id "thumbnails", class (sizeToString model.chosenSize) ]
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
+        ]
 
 main =
-  Html.beginnerProgram
-      { model = initialModel
-      , view = view
-      , update = update
-      }
+    Html.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
