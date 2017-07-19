236c6d0cb85a41eb48398594b766a2ecd0f68e46
Sid Ngeth <sngeth@gmail.com>
2017-07-19 15:39:39 -0400

    Unit test for decoding default title

---
+++ tests/PhotoGrooveTests.elm
@@ -0,0 +1,18 @@
+module PhotoGrooveTests exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, list, int, string)
+import Test exposing (..)
+import PhotoGroove exposing (..)
+import Json.Decode exposing (decodeString)
+
+
+decoderTest: Test
+decoderTest =
+  test "title defaults to (untitled)" <|
+    \_ ->
+      """{"url": "fruits.com", "size": 5}"""
+        |> decodeString photoDecoder
+        |> Result.map .title
+        |> Expect.equal
+          (Ok "(untitled)")
